31a9848457261a12a466ef19d033ab8362edca15
Santu Mahapatra <santumahapatra@gmail.com>
2014-05-22 10:11:42 +0530

    Tutorial 3: Adding details.html template and adding 404 for details view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -9,7 +10,11 @@ def index(request):
   return render(request, 'polls/index.html', context)
 
 def detail(request, poll_id):
-  return HttpResponse("You're looking at poll %s." % poll_id)
+  try:
+    poll = Poll.objects.get(pk=poll_id)
+  except Poll.DoesNotExist:
+    raise Http404
+  return render(request, 'polls/detail.html', {'poll': poll})
 
 def results(request, poll_id):
   return HttpResponse("You're looking at the results of poll %s." % poll_id)
