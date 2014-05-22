4e108995e012bf70abcb09ea71ace07a7b51a59d
Santu Mahapatra <santumahapatra@gmail.com>
2014-05-22 08:44:06 +0530

    Tutorial 2: Adding fieldsets to Poll page

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from polls.models import Poll
 
 class PollAdmin(admin.ModelAdmin):
-  fields = ['pub_date', 'question']
+  fieldsets = [
+    (None,               {'fields': ['question']}),
+    ('Date information', {'fields': ['pub_date']}),
+  ]
 
 admin.site.register(Poll, PollAdmin)
