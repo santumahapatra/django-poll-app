d6a73c08ab2d51839306afb1d772ff401643d83c
Santu Mahapatra <santumahapatra@gmail.com>
2014-05-22 11:26:44 +0530

    Tutorial 4: Adding form to detail.html and updating the vote view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
-from polls.models import Poll
+from polls.models import Poll, Choice
 
 def index(request):
   latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
@@ -16,4 +17,16 @@ def results(request, poll_id):
   return HttpResponse("You're looking at the results of poll %s." % poll_id)
 
 def vote(request, poll_id):
-  return HttpResponse("You're voting on poll %s." % poll_id)
+  p = get_object_or_404(Poll, pk=poll_id)
+  try:
+    selected_choice = p.choice_set.get(pk=request.POST['choice'])
+  except (KeyError, Choice.DoesNotExist):
+    # Redisplay the poll voting form.
+    return render(request, 'polls/detail.html', {
+      'poll': p,
+      'error_message': "You didn't select a choice.",
+      })
+  else:
+    selected_choice.votes += 1
+    selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
